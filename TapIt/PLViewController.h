37816c4506c92c32238be17a32636f0e9cb9ecd2
Gabriel Afonso <afonso@divux.com>
2014-06-03 22:27:44 -0430

    Initial Commit

---
+++ TapIt/PLViewController.h
@@ -0,0 +1,13 @@
+//
+//  PLViewController.h
+//  TapIt
+//
+//  Created by Gabriel Afonso on 6/3/14.
+//  Copyright (c) 2014 parallel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLViewController : UIViewController
+
+@end
---
+++ TapItTests/TapItTests.m
@@ -0,0 +1,34 @@
+//
+//  TapItTests.m
+//  TapItTests
+//
+//  Created by Gabriel Afonso on 6/3/14.
+//  Copyright (c) 2014 parallel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TapItTests : XCTestCase
+
+@end
+
+@implementation TapItTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
